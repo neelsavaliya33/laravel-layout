8e44c5fbd787a11f977f99c27ce5e2f8870bfbd9
Neel <neelsixty13@gmail.com>
2021-12-10 13:37:51 +0530

    first commit

---
+++ app/Http/Livewire/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Login extends Component
+{
+    public function render()
+    {
+        return view('livewire.login')
+            ->extends('layouts.main')
+            ->section('content')
+            ->layoutData([
+                'sidebar' => false,
+                'title' => 'Login'
+            ]);
+    }
+}
